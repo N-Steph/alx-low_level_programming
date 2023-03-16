d9366888e86890c157931bde67b6e6ffaa965c2a
nouaffo <snouaffo@gmail.com>
2023-03-16 10:37:23 +0000

    printing on standard output using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This program prints a message to standard output
+ * using printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is piece of art,\n");
+
+	return (0);
+}
