17c6e8a0308893be3d8e421f0c92dd51ee31d946
nouaffo <snouaffo@gmail.com>
2023-03-16 19:36:50 +0000

    use of the puts funtion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints a message to the stdout
+ * using puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
