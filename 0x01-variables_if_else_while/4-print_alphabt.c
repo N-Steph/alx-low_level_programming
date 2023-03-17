99765abb13a6eaf2179c1c724e2baa19d820ebc3
nouaffo <snouaffo@gmail.com>
2023-03-17 15:19:51 +0000

    use putchar to print alphabet, with some exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints out the alphabet
+ * using the putchar function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch < 123)
+	{
+		if (ch != 113 || ch != 101)
+			putchar(ch);
+		ch++;
+	}
+	ch = 10;
+	putchar(ch);
+	return (0);
+}
