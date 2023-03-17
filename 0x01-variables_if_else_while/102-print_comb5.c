0e7aa5ce2c9fef83a06ce95ec4849ca6070d63df
nouaffo <snouaffo@gmail.com>
2023-03-17 20:53:29 +0000

    Making use of 4 nested for loops to print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This prints all possible combinations of two-digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if (l > j)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(32);
+						putchar(k);
+						putchar(l);
+						if (i != 57 || j != 57 || k != 57 || l != 57)
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
