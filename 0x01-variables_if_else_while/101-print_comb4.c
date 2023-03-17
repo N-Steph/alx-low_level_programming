72eb48697777cfd04590e126226b02074d0090ff
nouaffo <snouaffo@gmail.com>
2023-03-17 20:11:18 +0000

    use three nested for loops and putchar function to print all different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints all possible different
+ * combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
