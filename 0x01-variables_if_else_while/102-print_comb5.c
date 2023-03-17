e2ea4ed27f5783b6f78133956668c65b22408571
nouaffo <snouaffo@gmail.com>
2023-03-17 22:01:25 +0000

    Change condition in first if

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,14 +21,14 @@ int main(void)
 			{
 				for (l = 48; l < 58; l++)
 				{
-					if (l > j)
+					if ((k >= i) && (l > j))
 					{
 						putchar(i);
 						putchar(j);
 						putchar(32);
 						putchar(k);
 						putchar(l);
-						if (i != 57 || j != 57 || k != 57 || l != 57)
+						if (i != 57 || j != 56 || k != 57 || l != 57)
 						{
 							putchar(44);
 							putchar(32);
