523b291bb4d5f25a1b89fab87eac16ec1298f5a4
nouaffo <snouaffo@gmail.com>
2023-03-21 18:18:13 +0000

    Make use of _putchar function whose declaration in the header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * This program prints _putchar using the function
+ * declaration in the header file
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10);
+	return (0);
+}
