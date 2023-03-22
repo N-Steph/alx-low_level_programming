61216e1e25acd6809f2003814b7511573fe65446
nouaffo <snouaffo@gmail.com>
2023-03-22 18:55:32 +0000

    Definition of add function declared in main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - add two integer
+ * @int1: first integer
+ * @int2: second integer
+ *
+ * This program prints returns the result of the addition
+ * of int1 and int2
+ *
+ * Return: result
+ */
+int add(int int1, int int2)
+{
+	int result;
+
+	result = int1 + int2;
+	return (result);
+}
