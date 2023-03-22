c55090f52852300a9a95e40560f84cdd580a5edb
nouaffo <snouaffo@gmail.com>
2023-03-22 19:35:21 +0000

    Definition of print_to_98 function declared in main.h

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print from any number to 98
+ * @n: number from where printing start
+ *
+ * This program prints n followed by subsequent
+ * numbers to 98, in ascending order
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int state;
+
+	state = 1;
+	while (state) 
+	{
+		while (n != 98)
+		{
+			printf("%d", n);
+			if (n < 98)
+				n++;
+			else if (n > 98)
+				n--;
+			if (n == 98)
+				printf(", %d\n", n);
+			else
+				printf(", ");
+		}
+		state = 0;
+	}
+}
