01659956debb8ce0f99d3d704c65fe3ba212644c
nouaffo <snouaffo@gmail.com>
2023-03-24 12:56:34 +0000

    Definition of function mul() declared in main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: first factor
+ * @b: second factor
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
