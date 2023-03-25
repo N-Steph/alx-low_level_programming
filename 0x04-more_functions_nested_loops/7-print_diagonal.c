16b616b91b151de3547c20b784a01ce5058360fe
nouaffo <snouaffo@gmail.com>
2023-03-25 03:16:19 +0000

    Correct comment and add another while loop with one constant and a variable

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * print_line - draw straight line in terminal
- * @n: number of times "_" is printed
+ * @n: number of times "\\" is printed
  *
- * This function draw a straight line in the terminal,
- * by printing "_" n number of times
+ * This function draw a diagonal line in the terminal,
+ * by printing "\\" n number of times.
  * Return: void
  */
 void print_diagonal(int n)
@@ -12,17 +12,27 @@ void print_diagonal(int n)
 	int counter;
 	int NEWLINE;
 	int BACKSLASH;
+	int  i;
+	int SPACE;
 
 	counter = 0;
 	NEWLINE = 10;
         BACKSLASH = 92;
+	i = 0;
+	SPACE = 32;
        	if (n > 0)
 	{
 		while (counter < n)
 		{
+			while (i < counter)
+			{
+				_putchar(SPACE);
+				i++;
+			}
 			_putchar(BACKSLASH);
 			_putchar(NEWLINE);
 			counter++;
+			i = 0;
 		}
 	}
 	_putchar(NEWLINE);
