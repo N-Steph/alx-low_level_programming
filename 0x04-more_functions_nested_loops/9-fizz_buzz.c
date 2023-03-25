ad14e933235d599f08679a235f8162405e71de06
nouaffo <snouaffo@gmail.com>
2023-03-25 19:43:01 +0000

    Solution to Fizz-Buzz exercise

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program print from 1 to 100,
+ * replacing multiples of 3 by Fizz,
+ * multiples of 5 by Buzz, and multiples
+ * of both 3 and 5 by FizzBuzz.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
