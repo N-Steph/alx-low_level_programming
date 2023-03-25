651a46bfe606a114f2e8547f17cb3d8502489ea4
nouaffo <snouaffo@gmail.com>
2023-03-25 03:01:35 +0000

    Define function print_diagonal declared in main.h

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_line - draw straight line in terminal
+ * @n: number of times "_" is printed
+ *
+ * This function draw a straight line in the terminal,
+ * by printing "_" n number of times
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int counter;
+	int NEWLINE;
+	int BACKSLASH;
+
+	counter = 0;
+	NEWLINE = 10;
+        BACKSLASH = 92;
+       	if (n > 0)
+	{
+		while (counter < n)
+		{
+			_putchar(BACKSLASH);
+			counter++;
+		}
+	}
+	_putchar(NEWLINE);
+}
