8f4900bcf57ba8b8a5c3ac923e679f16c89419f7
nouaffo <snouaffo@gmail.com>
2023-03-28 04:27:18 +0000

    Definition of reset_to_98 function declared in main.h

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - update value of variable through pointer
+ * @n: pointer storing the address of variable to update.
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
