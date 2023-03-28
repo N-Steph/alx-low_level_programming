743f91deb536920a303be7e8f954410647f91f41
nouaffo <snouaffo@gmail.com>
2023-03-28 19:13:36 +0000

    Definition of print_array function declared in main.h

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers.
+ * @a: pointer to the first element of array.
+ * @n: number of array element to be printed.
+ *
+ * This function prints n integer of an array starting
+ * from the first element.
+ *
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int counter;
+	/*int len;*/
+
+	counter = 0;
+	while (counter < n)
+	{
+		printf("%d", *a);
+		a += 1;
+		counter++;
+		if (counter < n)
+			printf(", ");
+	}
+	printf("\n");
+	/*
+	if (n <= len)
+	{
+		while (counter < n)
+		{
+			printf("%d", *a);
+			a += 1;
+			counter++;
+			if (counter < n)
+				printf(", ");
+		}
+	}
+	*/
+}
