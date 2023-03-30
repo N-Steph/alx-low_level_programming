31e33be70f5193737b1c6c13a67dd1edee827b74
nouaffo <snouaffo@gmail.com>
2023-03-30 10:58:23 +0000

    Definition of cap_string function declared in main.h

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize every word in a string
+ * @s: pointer to first character of string
+ *
+ * Return: s;
+ */
+char *cap_string(char *s)
+{
+	char word_separator[13] = {' ', '\t', '\n', ',', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+	char *ptrs;
+	int i;
+
+	ptrs = s;
+	if (*ptrs >= 97 && *ptrs <= 122)
+		*ptrs = *ptrs - 32;
+	for (i = 0; i < 13; i++)
+	{
+		while (*ptrs != '\0')
+		{
+			if (*ptrs == word_separator[i])
+			{
+				ptrs++;
+				if (*ptrs != '\0' && (*ptrs >= 97 && *ptrs <= 122))
+					*ptrs = *ptrs - 32;
+			}
+			ptrs++;
+		}
+		ptrs = s;
+	}
+	return (s);
+}
