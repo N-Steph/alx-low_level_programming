6e3c45bfafb3a3a21790340dac858473eeb820fa
nouaffo <snouaffo@gmail.com>
2023-03-30 13:59:17 +0000

    Remove if else statement before the while loop. Remove unused variables Add an if statement inside the while loop.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,29 +14,26 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	char *ptrdest;
 	char *ptrsrc;
-	int len;
 	int counter;
-	int number_of_characters;
+	int len;
 
 	ptrdest = dest;
 	ptrsrc = src;
-	len = 0;
 	counter = 0;
+	len = 0;
 	while (*ptrsrc != '\0')
 	{
 		ptrsrc += 1;
 		len++;
 	}
-	if (len < n)
-		number_of_characters = len;
-	else
-		number_of_characters = n;
-	while (counter < number_of_characters)
+	while (counter < n)
 	{
 		*ptrdest = *src;
 		ptrdest += 1;
 		src += 1;
 		counter++;
+		if (counter > len)
+			src -= 1;
 	}
 	if (*src == '\0')
 		*ptrdest = *src;
