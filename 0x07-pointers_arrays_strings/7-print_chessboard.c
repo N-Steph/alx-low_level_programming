5f7d3163b538856f3dd180a19e84e9b127c081c6
nouaffo <snouaffo@gmail.com>
2023-04-04 10:22:34 +0000

    Definition of print_chessboard funtion declared in main.h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @: pointer to first array index
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i;
+	unsigned int j;
+	char value;
+	int NEWLINE;
+
+	NEWLINE = 10;
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			value = *(*(a + i) + j);
+			_putchar(value);
+		}
+		_putchar(NEWLINE);
+	}
+}
