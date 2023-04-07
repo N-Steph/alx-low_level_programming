cedc6162d4bbd29bad897861a7e30c8b8b699268
nouaffo <snouaffo@gmail.com>
2023-04-07 20:29:35 +0000

    Add prototype of is_palindrome function, mv_to_last_char function, and is_palindrome_helper function

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,6 @@ int sqrt_help(int n, int sqrt);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int prime_num_helper(int n, int factor, int num_of_factor);
+int is_palindrome(char *s);
+int is_palindrome_helper(char *s1, char *s2);
+char *mv_to_last_char(char *s);
