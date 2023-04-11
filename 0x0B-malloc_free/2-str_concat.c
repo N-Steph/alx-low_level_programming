335b41cf58c700044cb2d295673de00772db55e2
nouaffo <snouaffo@gmail.com>
2023-04-11 11:57:06 +0000

    Definition of str_concat function declared in main.h

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenate two string
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: pointer to new allocated memory
+ * containing the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1;
+	int size2;
+	int size;
+	char *ptr_to_cat_mem;
+
+	if (s1 == 0)
+		size1 = 0;
+	else
+		size1 = strlen(s1);
+	if (s2 == 0)
+		size2 = 0;
+	else
+		size2 = strlen(s2);
+	size = size1 + size2;
+	ptr_to_cat_mem = malloc(sizeof(char) * (size + 1));
+	if (ptr_to_cat_mem == 0)
+		return (ptr_to_cat_mem);
+	ptr_to_cat_mem = strcpy(ptr_to_cat_mem, s1);
+	ptr_to_cat_mem = strcat(ptr_to_cat_mem, s2);
+	return (ptr_to_cat_mem);
+}
