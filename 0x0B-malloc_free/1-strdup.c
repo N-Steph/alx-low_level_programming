ec3e56ea4f16ab89c4abeb30ccd17c88c3abf3f3
nouaffo <snouaffo@gmail.com>
2023-04-11 11:09:34 +0000

    Definition of _strdup function declared in main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strdup - copy a string into another memory area
+ * @str: pointer to string
+ *
+ * Return: pointer to memory location where string
+ * was copied (Success), NULL if str=NULL or if
+ * insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	int size;
+	char *ptr_to_cp_mem;
+	char *ptr_temp;
+	int i;
+
+	size = strlen(str);
+	ptr_to_cp_mem = malloc(sizeof(char) * size);
+	if (ptr_to_cp_mem == 0)
+		return (ptr_to_cp_mem);
+	ptr_temp = ptr_to_cp_mem;
+	for (i = 0; i < (size + 1); i++)
+	{
+		*ptr_temp = *str;
+		ptr_temp++;
+		str++;
+	}
+	return (ptr_to_cp_mem);
+}
