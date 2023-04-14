3e7b9fb37cc33394ff39a57a3a564171e3094ea3
nouaffo <snouaffo@gmail.com>
2023-04-14 12:29:05 +0000

    Definition of string_nconcat function declared in main.h

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenate two string
+ * @s1: pointer to first string
+ * @s2: pointer to second sring
+ * @n: number of character of s2 to concatenate
+ * to s1
+ *
+ * Description: This function concatenate at most n
+ * bytesof s2 to s2 in another memeory location
+ *
+ * Return: pointer to allocated memory location
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr_char;
+	unsigned int size_nconcat;
+
+	size_nconcat = 0;
+	if (s1 == 0)
+		size_nconcat += 0;
+	else
+		size_nconcat += strlen(s1);
+	if (s2 == 0)
+		size_nconcat += 0;
+	else
+		size_nconcat += strlen(s2);
+
+	ptr_char = malloc(sizeof(char) * (size_nconcat + 1));
+	if (ptr_char == 0)
+		return (0);
+	strcpy(ptr_char, s1);
+	strncat(ptr_char, s2, n);
+	return (ptr_char);
+}
