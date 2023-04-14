d63d18250460015b6cd0f3f8a52a1706a7fc535f
nouaffo <snouaffo@gmail.com>
2023-04-14 13:30:09 +0000

    Definition of _calloc function declared in main.h

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate memory of an array
+ * @nmemb: number of array element
+ * @size: number of bytes of an element
+ *
+ * Return: pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	int *ptr_temp;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	ptr = malloc(size * nmemb);
+	if (ptr == 0)
+		return (0);
+	for (i = 0; i < nmemb; i++)
+	{
+		*ptr_temp = 0;
+		ptr_temp += size;
+	}
+	return (ptr);
+}
