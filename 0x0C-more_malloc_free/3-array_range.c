3f8634b5c8db06a951f2955c8d345e6027cfb1bc
nouaffo <snouaffo@gmail.com>
2023-04-15 02:41:31 +0000

    Remove _calloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	nmemb = (max - min) + 1;
 	if (max < min)
 		return (0);
-	ptr_int = _calloc(nmemb, sizeof(int));
+	ptr_int = malloc(sizeof(int) * nmemb);
 	if (ptr_int == 0)
 		return (0);
 	ptr_temp = ptr_int;
@@ -31,33 +31,4 @@ int *array_range(int min, int max)
 		ptr_temp += 1;
 	}
 	return (ptr_int);
-}
-
-/**
- * _calloc - Allocate memory of an array
- * @nmemb: number of array element
- * @size: number of bytes of an element
- *
- * Return: pointer to array
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *ptr;
-	int *ptr_temp;
-	unsigned int i;
-
-	if (nmemb == 0 || size == 0)
-		return (0);
-	if (size * nmemb > UINT_MAX)
-		return (0);
-	ptr = malloc(size * nmemb);
-	if (ptr == 0)
-		return (0);
-	ptr_temp = ptr;
-	for (i = 0; i < nmemb * size; i++)
-	{
-		*ptr_temp = 0;
-		ptr_temp += size;
-	}
-	return (ptr);
 }
