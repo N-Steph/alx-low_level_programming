9ef03067aa060b684f478b828446a161ac938632
nouaffo <snouaffo@gmail.com>
2023-04-16 04:44:02 +0000

    Definition of _realloc function declared in main.h

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - changes the size of a memory block
+ * @ptr: pointer to memory block to change size
+ * @old_size: size of memory block to change size
+ * @new_size: size of memory block to to change to
+ *
+ * Return: pointer to memory block of size new_size
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ptr_new;
+
+	if (ptr == 0 && new_size != 0)
+	{
+		ptr_new = malloc(new_size);
+		return (ptr_new);
+	}
+	else if (new_size == 0 && ptr != 0)
+	{
+		free(ptr);
+		return (0);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else
+	{
+		ptr_new = malloc(new_size);
+		if (old_size != 0)
+			ptr_new = memcpy(ptr_new, ptr, old_size);
+		return (ptr_new);
+	}
+}
