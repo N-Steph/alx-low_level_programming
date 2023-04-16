2d9cc4dc0c83a619d15d63a02485c7ad5e196463
nouaffo <snouaffo@gmail.com>
2023-04-16 05:14:04 +0000

    Add else if statement

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,7 +24,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (0);
 	}
-	else if (new_size == old_size && new_size != 0 && old_size != 0 )
+	else if (new_size == 0 && ptr == 0)
+	{
+		return (0);
+	}
+	else if (new_size == old_size && new_size != 0 && old_size != 0)
 	{
 		return (ptr);
 	}
