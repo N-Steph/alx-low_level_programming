ec844faee1652cf65cb3b210cd6122a62a38e106
nouaffo <snouaffo@gmail.com>
2023-04-17 21:59:47 +0000

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr_new_dog = malloc(sizeof(dog_t));
 	if (ptr_new_dog == 0)
 		return (0);
-	(*ptr_new_dog).name = name;
-	(*ptr_new_dog).owner = owner;
+	if (name == NULL)
+		(*ptr_new_dog).name = "(nil)";
+	else
+		(*ptr_new_dog).name = name;
+	if (owner == NULL)
+		(*ptr_new_dog).owner = "(nil)";
+	else
+		(*ptr_new_dog).owner = owner;
 	(*ptr_new_dog).age = age;
 	return (ptr_new_dog);
 }
