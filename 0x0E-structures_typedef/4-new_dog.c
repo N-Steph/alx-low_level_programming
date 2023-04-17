0fa940ce802b77940336858736e1165f18585f85
nouaffo <snouaffo@gmail.com>
2023-04-17 12:35:53 +0000

    Definition of new_dog function(update)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr_new_dog;
 	dog_t *ptr_temp;
+	char *ptr_name;
+	char *ptr_owner;
 
-	if (name == 0 || owner == 0)
-		return (0);
 	ptr_new_dog = malloc(sizeof(char *) * 2 + sizeof(float));
-	if (ptr_new_dog == 0)
+	ptr_name = malloc(sizeof(char) * (strlen(name) + 1));
+	ptr_owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (ptr_new_dog == 0 || ptr_name == 0 || ptr_owner == 0)
 		return (0);
 	ptr_temp = ptr_new_dog;
-	(*ptr_temp).name = name;
-	(*ptr_temp).owner = owner;
+	strcpy(ptr_name, name);
+	strcpy(ptr_owner, owner);
+	(*ptr_temp).name = ptr_name;
+	(*ptr_temp).owner = ptr_owner;
 	(*ptr_temp).age = age;
 	return (ptr_new_dog);
 }
