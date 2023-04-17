26898726fcb6a200e06a3268b1a29d9266a548ab
nouaffo <snouaffo@gmail.com>
2023-04-17 22:11:38 +0000

    Definition of free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to variable of type dog_t
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
