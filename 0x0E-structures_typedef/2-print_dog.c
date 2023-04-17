1ce7e028a3653dc7181e6340a16fdc827d2f78c2
nouaffo <snouaffo@gmail.com>
2023-04-17 11:09:01 +0000

    Remove if...else statement

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,9 +16,6 @@ void print_dog(struct dog *d)
 	if (d->owner == 0)
 		d->owner = "(nil)";
 	printf("Name: %s\n", d->name);
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
 }
