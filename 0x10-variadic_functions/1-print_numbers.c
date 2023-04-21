7ff3711f232dcf6ee8faafbd02f582aea77df664
nouaffo <snouaffo@gmail.com>
2023-04-21 00:50:17 +0000

    Add another if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,6 +18,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(opt_arg, n);
 	if (separator == NULL)
 		return;
+	if (n == 0)
+		return;
 	for (i = 0; i < n - 1; i++)
 		printf("%d%s", va_arg(opt_arg, int), separator);
 	printf("%d\n", va_arg(opt_arg, int));
