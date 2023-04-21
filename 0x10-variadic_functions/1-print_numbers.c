7a96e109145210e03866471d22c69b176e75316a
nouaffo <snouaffo@gmail.com>
2023-04-21 01:00:31 +0000

    modify algorithm

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,12 +16,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list opt_arg;
 
 	va_start(opt_arg, n);
-	if (separator == NULL)
-		return;
-	if (n == 0)
-		return;
-	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(opt_arg, int), separator);
-	printf("%d\n", va_arg(opt_arg, int));
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(opt_arg, int));
+			if (i != n - 1)
+				printf("%s", separator);
+		}
+	}
 	va_end(opt_arg);
+	printf("\n");
 }
