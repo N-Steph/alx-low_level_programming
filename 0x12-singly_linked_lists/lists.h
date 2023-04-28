d76e3fe5fd6f321467e65d10474a22eb57a8599d
nouaffo <snouaffo@gmail.com>
2023-04-28 17:23:30 +0000

    Add free_list prototype function

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,4 +21,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 unsigned int _strlen(const char *s);
 list_t *move_to_last_node(list_t *h);
+void free_list(list_t *head);
 #endif /* LIST_H */
