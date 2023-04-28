e4e2296f7cb7b7fac728aafcb903ac4a6d60e28e
nouaffo <snouaffo@gmail.com>
2023-04-28 15:24:29 +0000

    Definition of list_len function

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - count the number of elements in a linked list.
+ * @h: pointer to head node
+ *
+ * Return: number of elements in a linked list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t num_nodes;
+
+	num_nodes = 0;
+	if (h == 0)
+		exit(1);
+	while (h != 0)
+	{
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
+}
