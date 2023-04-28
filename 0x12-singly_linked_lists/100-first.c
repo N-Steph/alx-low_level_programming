13bd9ad647e64c2ff91947ed49b47c8adfb6f95d
nouaffo <snouaffo@gmail.com>
2023-04-28 18:15:16 +0000

    Definition of b_main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * b_main() - prints message before main() execution.
+ *
+ * Return: nothing
+ */
+void b_main()
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
