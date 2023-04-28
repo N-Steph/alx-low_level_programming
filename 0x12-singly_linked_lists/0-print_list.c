d14ee9b13abf85990bf5f9a4de59cd7019c3b7ca
nouaffo <snouaffo@gmail.com>
2023-04-28 15:08:28 +0000

    Definition of print_list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list_t list.
+ * @h: pointer to the head node
+ *
+ * Return: number of node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num_nodes;
+
+	num_nodes = 0;
+	if (h == 0)
+		exit(1);
+	while (h != 0)
+	{
+		if (h->str == 0)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
