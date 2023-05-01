06c35f4a7e340e2634342719ce5568140d165bab
nouaffo <snouaffo@gmail.com>
2023-05-01 14:50:55 +0000

    Definition of pop_listint function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - delete head node
+ * @head: address of the pointer to head node
+ *
+ * Return: head's node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr_temp;
+	int n;
+
+	ptr_temp = *head;
+	n = ptr_temp->n;
+	*head = (*head)->next;
+	free(ptr_temp);
+	return (n);
+}
