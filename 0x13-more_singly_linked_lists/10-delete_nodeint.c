0dcc80c47147a8af429478a1fc172f85617c64c7
nouaffo <snouaffo@gmail.com>
2023-05-01 17:15:05 +0000

    Definition of delete_nodeint_at_index function

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,141 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index- delete node at a given position
+ * @head: address of pointer to head node
+ * @index: position of node to delete
+ *
+ * Return: data of node deleted
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *ptr_idx_prev;
+	listint_t *ptr_idx_curr;
+	listint_t *ptr_idx_later;
+
+	if (head == NULL || *head == NULL || index >= listint_len(*head))
+		return (-1);
+	if (index == 0)
+	{
+		pop_listint(head);
+		return (1);
+	}
+	else if (index == (listint_len(*head) - 1))
+	{
+		delete_last_node(head);
+		return (1);
+	}
+	ptr_idx_prev = get_nodeint_at_index(*head, index - 1);
+	ptr_idx_curr = get_nodeint_at_index(*head, index);
+	ptr_idx_later = get_nodeint_at_index(*head, index + 1);
+
+	ptr_idx_prev->next = ptr_idx_later;
+	free(ptr_idx_curr);
+	return (1);
+}
+
+
+/**
+ * listint_len - count the number of nodes in linked listint_t list
+ * @h: pointer to head node
+ *
+ * Return: number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len_list;
+
+	len_list = 0;
+	if (h == NULL)
+		return (len_list);
+	do {
+		len_list++;
+		h = h->next;
+	} while (h != NULL);
+	return (len_list);
+}
+
+
+/**
+ * get_nodeint_at_index - get address of nth node.
+ * @head: pointer to head node
+ * @index: nth node to find
+ *
+ * Return: address of nth node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			break;
+	}
+	return (head);
+}
+
+
+/**
+ * delete_last_node- add new node at end of linked listint_t list.
+ * @head: address of pointer to head node
+ *
+ * Return: data of deleted node 
+ */
+int delete_last_node(listint_t **head)
+{
+	listint_t *ptr_temp;
+
+	ptr_temp = *head;
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	ptr_temp = mv_node_before_last(*head);
+	free(ptr_temp->next);
+	ptr_temp->next = NULL;
+	return (0);
+}
+
+/**
+ * mv_last_node - get address of last node of listint_t list
+ * @s: pointer to head node
+ *
+ * Return: address of last node
+ */
+listint_t *mv_node_before_last(listint_t *s)
+{
+	int n;
+	int i;
+
+	n = listint_len(s);
+	i = 0;
+	while (i < n - 2)
+		s = s->next;
+	return (s);
+}
+
+
+/**
+ * pop_listint - delete head node
+ * @head: address of the pointer to head node
+ *
+ * Return: head's node data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr_temp;
+	int n;
+
+	ptr_temp = *head;
+	n = ptr_temp->n;
+	*head = (*head)->next;
+	free(ptr_temp);
+	return (n);
+}
