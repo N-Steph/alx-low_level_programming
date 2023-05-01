868bdc16989e8a974ad46cb6cef6d6354c734490
nouaffo <snouaffo@gmail.com>
2023-05-01 10:50:39 +0000

    Definition of listint_len function

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint - count the number of nodes in linked listint_t list
+ * @h: pointer to head node
+ *
+ * Return: number of nodes.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len_list;
+
+	len_list = 0;
+	if (h == NULL)
+		return (len_list);
+	do {
+		len_list++;
+		h = h->next;
+	} while (h != NULL);
+	return (len_list);
+}
