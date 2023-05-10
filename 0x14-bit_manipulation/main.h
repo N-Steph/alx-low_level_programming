0cc98c37db56ed68c2ceb92b0a5c6de6a7857316
nouaffo <snouaffo@gmail.com>
2023-05-10 20:14:05 +0000

    Add num_significant_bit prototype function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+unsigned int num_significant_bit(unsigned long int);
 #endif /* MAIN_H */
