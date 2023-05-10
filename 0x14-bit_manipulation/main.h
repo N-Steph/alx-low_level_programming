4936dd6d9066846698f7f5e16faf9b2c471a1fa2
nouaffo <snouaffo@gmail.com>
2023-05-10 22:21:07 +0000

    Add prototype of reverse_bit_sequence function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,4 +4,5 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned int num_significant_bit(unsigned long int);
+void reverse_bit_sequence(unsigned long int *bit_ptr);
 #endif /* MAIN_H */
