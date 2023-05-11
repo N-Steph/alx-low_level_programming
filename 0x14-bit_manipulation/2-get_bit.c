03d46c07579a051df1ac6c314b51f18184ce0a61
nouaffo <snouaffo@gmail.com>
2023-05-11 10:16:15 +0000

    Update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,39 +9,17 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int num_signif_bit;
 	unsigned int i;
 	int index_value;
 
-	num_signif_bit = num_significant_bit(n);
-	if (index > (num_signif_bit - 1))
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	i = 0;
-	while (i < (num_signif_bit - index))
+	while (i < index)
 	{
 		n >>= 1;
 		i++;
 	}
 	index_value = n & 1;
 	return (index_value);
-}
-
-/**
- * num_significant_bit - count significant bit of a number
- * @n: number to count its significant bit
- *
- * Return: number of significant bits counter
- */
-
-unsigned int num_significant_bit(unsigned long int n)
-{
-	unsigned int num_bit;
-
-	num_bit = 0;
-	while (n != 0)
-	{
-		n = n >> 1;
-		num_bit++;
-	}
-	return (num_bit);
 }
