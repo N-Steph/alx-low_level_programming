d671b085383e5577ff503d5dadb99e8d1ef696b1
nouaffo <snouaffo@gmail.com>
2023-05-11 10:30:38 +0000

    Add prototpye of set_bit function

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,4 +6,5 @@ void print_binary(unsigned long int n);
 unsigned int num_significant_bit(unsigned long int);
 void reverse_bit_sequence(unsigned long int *bit_ptr);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif /* MAIN_H */
