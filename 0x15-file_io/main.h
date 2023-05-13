7f06ec72451a242f3c7dfa7d40d05b2ac3da71ba
nouaffo <snouaffo@gmail.com>
2023-05-13 13:04:47 +0000

    Add create_file function prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,4 +3,5 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 size_t print_buffer(int, char *, size_t);
+int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
