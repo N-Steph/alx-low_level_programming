c59d9d701f3f57e78c107ec36a31c2d4c154bd51
nouaffo <snouaffo@gmail.com>
2023-05-13 15:16:18 +0000

    Update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fdes = open(filename, O_APPEND|O_WRONLY);
+	fdes = open(filename, O_APPEND | O_WRONLY);
 	if (fdes == -1)
 		return (-1);
 	if (text_content == NULL)
