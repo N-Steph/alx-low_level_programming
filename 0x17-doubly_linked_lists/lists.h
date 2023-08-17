d83141f5a826fe513e15c0b4bbc14fb967024749
nouaffo <snouaffo@gmail.com>
2023-08-17 15:57:46 +0000

    "Changed LIST_H to LISTS_H"

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -26,4 +26,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-#endif /* LIST_H */
+#endif /* LISTS_H */
