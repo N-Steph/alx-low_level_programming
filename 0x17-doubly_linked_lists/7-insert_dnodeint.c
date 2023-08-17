2a339d16c5db248802515c2947dacabab54471c3
nouaffo <snouaffo@gmail.com>
2023-08-17 18:56:12 +0000

    "Update"

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *node;
+	dlistint_t *new_node, *node, *prev;
 	unsigned int counter;
 
 	if (h == NULL)
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node = add_dnodeint_end(h, n);
 		return (new_node);
 	}
-	else if (node == NULL && counter != idx)
+	else if (node == NULL && (counter + 1) < idx)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
@@ -42,5 +42,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = node;
 	new_node->prev = node->prev;
 	new_node->n = n;
+	prev = node->prev;
+	prev->next = new_node;
 	return (new_node);
 }
