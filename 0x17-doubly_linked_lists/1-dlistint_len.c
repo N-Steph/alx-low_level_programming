bb29e00c91f7a7ec43ebdcc7e36eb14242770879
nouaffo <snouaffo@gmail.com>
2023-08-17 15:31:10 +0000

    "Counting the number of elements in a doubly linked list"

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts the number of elements in a linked dlistint_t list
+ * @h: address of head node
+ *
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_node;
+
+	if (h == NULL)
+		return (0);
+	num_node = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		++num_node
+	}
+	return (num_node);
+}
