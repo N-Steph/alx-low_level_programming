37ef72fd9c675e4679f0a48aa88844974507eb5e
nouaffo <snouaffo@gmail.com>
2023-08-18 13:45:04 +0000

    "Changed the whole code"

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,36 +9,68 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *node, *present, *prev, *next;
-	unsigned int counter;
+	dlistint_t *h;
+	size_t d_len, counter;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	node = *head;
-	if (index == 0)
+	d_len = dlistint_len(*head);
+	if (d_len == 1 && index == 0)
 	{
-		head = (*head)->next;
-		(*head)->prev = NULL;
-		free(node);
+		free(*head);
+		(*head) = NULL;
 		return (1);
 	}
-	counter = 0;
-	while (node != NULL)
-	{
-		if (counter == index)
-			break;
-		++counter;
-		node = node->next;
-	}
-	if (node == NULL)
+	if (d_len == 1 && index != 0)
 		return (-1);
-	present = node;
-	prev = node->prev;
-	next = node->next;
-	prev->next = next;
-	next->prev = prev;
-	present->next = NULL;
-	present->prev = NULL;
-	free(present);
+	if (d_len > 1 && index == 0)
+	{
+		h = *head;
+		*head = (*head)->next;
+		(*head)->prev = NULL;
+		h->next = NULL;
+		free(h);
+		return (1);
+	}
+	if (d_len > 1 && (index > 0 && index < d_len))
+	{
+		counter = 0;
+		h = *head;
+		while (h != NULL)
+		{
+			if (counter == index)
+				break;
+			++counter;
+			h = h->next;
+		}
+		(h->prev)->next = h->next;
+		(h->next)->prev = h->prev;
+		free(h);
+		return (1);
+	}
+	if (d_len > 1 && index >= d_len)
+		return (-1);
+	 
 	return (1);
+}
+
+/**
+ * dlistint_len - counts the number of elements in a linked dlistint_t list
+ * @h: address of head node
+ *
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_node;
+
+	if (h == NULL)
+		return (0);
+	num_node = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		++num_node;
+	}
+	return (num_node);
 }
