ae064925b993540c809f775323a7e7114b9cf3ef
nouaffo <snouaffo@gmail.com>
2023-09-09 13:36:54 +0000

    Added the data structure for the hash table nodes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,4 +1,19 @@
 #ifndef HASH_TABLES_H
-#define
+#define HASH_TABLES_H
+
+/*
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the list
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
 
 #endif /*HASH_TABLES_H*/
