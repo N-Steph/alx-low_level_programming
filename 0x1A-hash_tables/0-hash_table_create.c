e863d2e0aa5f42590f9f7b4d3e899ed3c96cb00e
nouaffo <snouaffo@gmail.com>
2023-09-09 19:16:19 +0000

    Definition of hash_table_create function

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t - Creates a hash table
+ * @size: size of the hash table
+ *
+ * Return: pointer to the created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hast_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = NULL;
+	return (table);
+}
