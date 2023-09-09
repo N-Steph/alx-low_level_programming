e00e74ed915b88bc93962bbd9628c3037b5ad709
nouaffo <snouaffo@gmail.com>
2023-09-09 21:36:26 +0000

    Update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->key = (char *)key;
 		node->value = (char *)value;
 		node->next = NULL;
+		ht->array[index] = node;
 	}
 	else
 	{
