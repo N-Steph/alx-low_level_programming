5708e5edf73bee00b4116cc85ad3ad518f4d195f
nouaffo <snouaffo@gmail.com>
2023-09-10 00:06:14 +0000

    Initialized memory allocated from head

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t*) * size);
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
+	}
+	i = 0;
+	array = table->array;
+	while (i < size)
+	{
+		array[i] = NULL;
+		i++;
 	}
 	return (table);
 }
