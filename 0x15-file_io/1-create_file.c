a5a6f06b62f543696c9074d9112391db5c8e6c5c
N-Steph <snouaffo@gmail.com>
2023-09-27 08:50:45 +0000

    closed the file in cate write() function returns -1 or doesn't write the correct number of bytes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,10 @@ int create_file(const char *filename, char *text_content)
 	text_len = _strlen(text_content);
 	char_written = write(fd, text_content, text_len);
 	if (char_written == -1 || char_written != text_len)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 
 	return (1);
