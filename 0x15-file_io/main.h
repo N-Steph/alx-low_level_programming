bc24c5ac3160efa1c4a628af80a739f859e2fe16
N-Steph <snouaffo@gmail.com>
2023-09-27 08:33:30 +0000

    Added function prototype of create_file() and _strlen

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 
 int _puchar(char c);
+int _strlen(char *s);
 ssize_t read_textfile(const char *filname, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
