562c47bbc782dd04b2eb535cf052618f65b19188
N-Steph <snouaffo@gmail.com>
2023-09-27 09:35:49 +0000

    added the prototype of append_text_to_file() function

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ int _puchar(char c);
 int _strlen(char *s);
 ssize_t read_textfile(const char *filname, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
