9e7d09af77875d3987c06b0d12e376ab0fca5da0
N-Steph <snouaffo@gmail.com>
2023-09-27 11:11:19 +0000

    Added some prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,11 +6,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int _puchar(char c);
 int _strlen(char *s);
 ssize_t read_textfile(const char *filname, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void error_closure(int fd);
+void error_create_write(char *);
+void error_exist_read(char *);
 
 #endif /* MAIN_H */
