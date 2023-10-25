74891459ade361b4bccb20d3466dcf480b5e3251
N-Steph <snouaffo@gmail.com>
2023-10-25 11:37:43 +0000

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,14 +8,22 @@ def island_perimeter(grid):
         grid: a list of list of integers
     """
     if len(grid) == 0 or len(grid) > 100:
-        return 
+        return 0
     perimeter = 0
+    start = 0
+    init = 0
     for side in grid:
         if len(side) > 100:
-            return
+            return 0
+        peri = 0
         for cell in side:
             if cell == 1 and perimeter == 0:
-                perimeter += 4
+                init = 1
+                peri += 4
             elif cell == 1 and perimeter != 0:
-                perimeter += 2
+                peri += 2
+        if init == 1 and peri == 0:
+            break
+        else:
+            perimeter += peri
     return perimeter
